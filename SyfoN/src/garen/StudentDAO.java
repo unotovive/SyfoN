8bce090993e10d68878ae17b4dc49e50e0290d1d
honoka <honoka@133.14.210.89>
2018-07-06 16:45:38 +0900

    Student仮完成

--- SyfoN/src/garen/StudentDAO.java
+++ SyfoN/src/garen/StudentDAO.java
@@ -16,7 +16,7 @@ public class StudentDAO {
 	final private static String driverClassName = "org.postgresql.Driver";
 	final private static String url = "jdbc:postgresql://localhost/" + dbname;
 
-	public Student getStudent(Student student) throws SQLException {
+	public Student getStudent(String studentID) throws SQLException {
 		// memberがDBにあるかどうかを調べる
 		Student result = new Student();
 		Connection connection;
@@ -27,7 +27,7 @@ public class StudentDAO {
 			connection = DriverManager.getConnection(url, user, password);
 			PreparedStatement pstmt = connection.prepareStatement(sql);
 
-			pstmt.setString(1, student.getStudentID());
+			pstmt.setString(1, studentID);
 			/*
 			pstmt.setInt(2, student.getGradeID());
 			pstmt.setString(3, student.getPassWord());
@@ -59,6 +59,62 @@ public class StudentDAO {
 
 			pstmt.setString(1, student.getStudentID());
 			pstmt.setString(2, student.getPassWord());
+
+			ResultSet resultSet = pstmt.executeQuery();
+			if (resultSet.next()) result = true;
+
+			resultSet.close();
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public boolean registerStudent(Student student) throws SQLException {
+		// memberがDBにあるかどうかを調べる
+		boolean result = false;
+		Connection connection;
+		String sql = "INSERT INTO student VALUES(?,?,?,?,?);";
+
+		try {
+			Class.forName(driverClassName);
+			connection = DriverManager.getConnection(url, user, password);
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+
+			pstmt.setString(1, student.getStudentID());
+			pstmt.setInt(2, student.getGradeID());
+			pstmt.setString(3, student.getPassWord());
+			pstmt.setString(4, student.getMailAddress());
+			pstmt.setString(5, student.getNickname());
+
+			ResultSet resultSet = pstmt.executeQuery();
+			if (resultSet.next()) result = true;
+
+			resultSet.close();
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public boolean updateStudent(Student student) throws SQLException {
+		// memberがDBにあるかどうかを調べる
+		boolean result = false;
+		Connection connection;
+		String sql = "UPDATE student SET password = ?,mailaddress = ?  WHERE studentID = ?;";
+
+		try {
+			Class.forName(driverClassName);
+			connection = DriverManager.getConnection(url, user, password);
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+
+
+			pstmt.setString(1, student.getPassWord());
+			pstmt.setString(2, student.getMailAddress());
+			pstmt.setString(3, student.getStudentID());
+
 
 			ResultSet resultSet = pstmt.executeQuery();
 			if (resultSet.next()) result = true;
--- SyfoN/src/garen/StudentManager.java
+++ SyfoN/src/garen/StudentManager.java
@@ -7,9 +7,23 @@ public class StudentManager {
 	private Student student;
 	private StudentDAO studentDAO;
 
-	public Student getStudent(Student studentID) throws SQLException{
+	public Student getStudent(String studentID) throws SQLException{
 		student = studentDAO.getStudent(studentID);
 		return student;
+	}
+	
+	public boolean registerStudent(Student student) throws SQLException{
+		boolean result = false;
+			result = studentDAO.registerStudent(student);
+		return result;
+
+	}
+	
+	public boolean editProf(Student student) throws SQLException{
+		boolean result = false;
+			result = studentDAO.updateStudent(student);
+		return result;
+
 	}
 
 
