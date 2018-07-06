59548d7839e9542ff4533aa9d8621350dcace7c7
honoka <honoka@133.14.210.89>
2018-07-06 16:57:58 +0900

    kousin

--- SyfoN/src/garen/StudentManager.java
+++ SyfoN/src/garen/StudentManager.java
@@ -11,14 +11,21 @@ public class StudentManager {
 		student = studentDAO.getStudent(studentID);
 		return student;
 	}
-	
+
+	public boolean check(Student student) throws SQLException{
+		boolean result = false;
+			result = studentDAO.check(student);
+		return result;
+
+	}
+
 	public boolean registerStudent(Student student) throws SQLException{
 		boolean result = false;
 			result = studentDAO.registerStudent(student);
 		return result;
 
 	}
-	
+
 	public boolean editProf(Student student) throws SQLException{
 		boolean result = false;
 			result = studentDAO.updateStudent(student);
