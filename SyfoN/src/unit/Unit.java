772eea0ab91beaafb40592e595a90abfb4f39bb1
chassep0t-man0 <ketti.keu.08@gmail.com>
2018-07-12 14:03:15 +0900

    #update

---
+++ SyfoN/src/unit/Unit.java
@@ -0,0 +1,27 @@
+package unit;
+
+public class Unit {
+	private String unitID;
+	private String unitName;
+	private int unitTani;
+
+	public String getUnitID() {
+		return unitID;
+	}
+	public void setUnitID(String unitID) {
+		this.unitID = unitID;
+	}
+	public String getUnitName() {
+		return unitName;
+	}
+	public void setUnitName(String unitName) {
+		this.unitName = unitName;
+	}
+	public int getUnitTani() {
+		return unitTani;
+	}
+	public void setUnitTani(int unitTani) {
+		this.unitTani = unitTani;
+	}
+
+}
