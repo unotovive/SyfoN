d1a4032ffcc8f9121881a269532e832ffa12f9f1
chassep0t-man0 <ketti.keu.08@gmail.com>
2018-07-13 19:12:24 +0900

    #update

---
+++ SyfoN/src/professorToLecture/ProfessorToLectureDAO.java
@@ -0,0 +1,12 @@
+package professorToLecture;
+
+public class ProfessorToLectureDAO {
+	final private static String dbname = "garen";   // データベース名
+	final private static String user = "knight";      // tutorialにアクセスできるユーザ
+	final private static String password = "terror"; // wspuserのパスワード
+	final private static String driverClassName = "org.postgresql.Driver";
+	final private static String url = "jdbc:postgresql://localhost/" + dbname;
+
+
+
+}
