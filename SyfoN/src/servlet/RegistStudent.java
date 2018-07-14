2a00753f20cc1f29f4af9a988d1a10a57d9e24a5
chassep0t-man0 <ketti.keu.08@gmail.com>
2018-07-14 16:18:58 +0900

    [fix]

--- SyfoN/src/servlet/RegistStudent.java
+++ SyfoN/src/servlet/RegistStudent.java
@@ -10,17 +10,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import student.Student;
+import student.StudentManager;
+
 /**
  * Servlet implementation class AddSystem
  */
-@WebServlet("/AddSystem")
-public class AddSystem extends HttpServlet {
+@WebServlet("/RegisterStudent")
+public class RegisterStudent extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AddSystem() {
+    public RegisterStudent() {
         super();
         // TODO Auto-generated constructor stub
     }
