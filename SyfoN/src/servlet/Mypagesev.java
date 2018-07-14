82a729c16b2eb4a72c5eedff89ec6f3afbf0d368
chassep0t-man0 <ketti.keu.08@gmail.com>
2018-07-14 17:02:50 +0900

    [fix]MyPageSev

--- SyfoN/src/servlet/Mypagesev.java
+++ SyfoN/src/servlet/Mypagesev.java
@@ -29,7 +29,7 @@ public class Mypagesev extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
-		getServletContext().getRequestDispatcher("/add.jsp").forward(request, response);
+		getServletContext().getRequestDispatcher("/Mypage.jsp").forward(request, response);
 
 		/*
 		Student student = new Student();
