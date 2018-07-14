5f7ea46c6c9a16f5a7a7a40c14770f4b470844f8
wkno115 <nm08gm04@ezweb.ne.jp>
2018-07-14 19:53:35 +0900

    [create]2Servlet

---
+++ SyfoN/src/servlet/CreateTable.java
@@ -0,0 +1,57 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class CreateTable
+ */
+@WebServlet("/CreateTable")
+public class CreateTable extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateTable() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String semester =request.getParameter("semester");
+
+		try {
+		HttpSession session = request.getSession();
+		
+		
+
+
+		getServletContext().getRequestDispatcher("/create.jsp").forward(request, response);
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	}
+
+}
---
+++ SyfoN/src/servlet/RegisterTimeTable.java
@@ -0,0 +1,62 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class RegisterTimeTable
+ */
+@WebServlet("/RegisterTimeTable")
+public class RegisterTimeTable extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterTimeTable() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String rev =request.getParameter("timetableid");
+
+		try {
+		HttpSession session = request.getSession();
+
+		boolean result = false;
+			result = .updateReview(rev);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		if (result) {
+			// レビューに成功したとき
+			getServletContext().getRequestDispatcher("/top.jsp").forward(request, response);
+		} else {
+			// ログインに失敗している場合はlogin.jspへ
+			getServletContext().getRequestDispatcher("/CreateTable").forward(request, response);
+			}
+		}
+	}
+
+}
