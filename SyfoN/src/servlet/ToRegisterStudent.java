4b94c9731b07a95aeaf2cbf4b6eced55c48361b0
chassep0t-man0 <ketti.keu.08@gmail.com>
2018-07-14 16:39:49 +0900

    [fix] ToRegisterStudent

--- SyfoN/src/servlet/ToRegisterStudent.java
+++ SyfoN/src/servlet/ToRegisterStudent.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ToAdSys
  */
-@WebServlet("/ToAdSys")
+@WebServlet("/ToRegisterStudent")
 public class ToRegisterStudent extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
