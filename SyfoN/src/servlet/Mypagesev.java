6cb8c08f0d094c6167d69caf1fed3cb65d5ee07b
chassep0t-man0 <ketti.keu.08@gmail.com>
2018-07-14 17:01:18 +0900

    [fix] MypageSev

--- SyfoN/src/servlet/Mypagesev.java
+++ SyfoN/src/servlet/Mypagesev.java
@@ -1,17 +1,12 @@
 package servlet;
 
 import java.io.IOException;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import model.Student;
-import model.StudentManager;
 
 /**
  * Servlet implementation class Mypagesev
@@ -33,6 +28,10 @@ public class Mypagesev extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+		getServletContext().getRequestDispatcher("/add.jsp").forward(request, response);
+
+		/*
 		Student student = new Student();
 		StudentManager mane = new StudentManager();
 		Student result =  new Student();
@@ -45,6 +44,7 @@ public class Mypagesev extends HttpServlet {
 
 		HttpSession session = request.getSession();
 		session.setAttribute("Myinfo", result);
+		*/
 
 	}
 
