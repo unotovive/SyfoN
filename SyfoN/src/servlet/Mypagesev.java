7179df6e50a558d654ba0d33e5b1f7660f9241c3
wkno115 <nm08gm04@ezweb.ne.jp>
2018-07-15 21:08:57 +0900

    [update]MyPageServ.java

--- SyfoN/src/servlet/Mypagesev.java
+++ SyfoN/src/servlet/Mypagesev.java
@@ -1,12 +1,19 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONObject;
+
+import student.Student;
 
 /**
  * Servlet implementation class Mypagesev
@@ -26,8 +33,28 @@ public class Mypagesev extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+		HttpSession session = request.getSession();
+
+		Student student=(Student)session.getAttribute("student");
+		Map<String, Map> studentMap=new HashMap<String,Map>();
+		Map<String,String> studentDataMap=new HashMap<String,String>();
+
+		studentDataMap.put("name", student.getNickName());
+		studentDataMap.put("id", student.getStudentID());
+		studentDataMap.put("grade", Integer.toString(student.getGradeID()));
+		studentDataMap.put("pass",student.getPassWord());
+		studentDataMap.put("mail", student.getMailAddress());
+
+		studentMap.put("student", studentDataMap);
+
+		//JSON形に変換
+		JSONObject studentJson=new JSONObject(studentMap);
+		System.out.println(studentJson);
+		session.setAttribute("studentJson", studentJson);
 
 		//トップページからマイページへ
 		getServletContext().getRequestDispatcher("/Mypage.jsp").forward(request, response);
@@ -52,6 +79,7 @@ public class Mypagesev extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
